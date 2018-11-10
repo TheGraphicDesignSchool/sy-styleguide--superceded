96062d3af4b24366c52462646fedf47b40c4ff46
Jean-Marc Giorgi <jmgiorgi@gmail.com>
2018-11-11 05:33:03 +1100

    setup //1

---
+++ src/app.js
@@ -0,0 +1,76 @@
+import React from "react";
+import Button from "react-ions/lib/components/Button";
+import Icon from "react-ions/lib/components/Icon";
+
+import Popover from 'react-ions/lib/components/Popover'
+import StyledDiv from 'react-ions/lib/components/StyledDiv'
+
+class ExampleBottomPopover extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    state = {
+        showing: false
+    }
+
+    togglePopover = () => {
+        this.setState({ showing: !this.state.showing })
+    }
+
+    getPopoverContent = () => (
+        <div className={localStyle['popover-wrapper']}>
+            <h1>Lorem ipsum dolor sit amet.</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur.</p>
+            <Button onClick={this.togglePopover} optClass={localStyle['popover-btn']}>Close</Button>
+        </div>
+    )
+
+    render = () => (
+        <StyledDiv css={{ display: 'flex', justifyContent: 'space-around' }}>
+            <Popover
+                showing={this.state.showing}
+                defaultPosition='bottom'
+                content={this.getPopoverContent()}
+                maxHeight='280px'
+                onRequestClose={this.togglePopover}
+            >
+                <Button onClick={this.togglePopover}>
+                    { this.state.showing.left ? 'Close' : 'Open' } Bottom popover
+                </Button>
+            </Popover>
+        </StyledDiv>
+    )
+}
+
+export default () => {
+  return (
+    <div>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <hr />
+      <Button optClass="secondary" collapse={true}>
+        <Icon name="md-add" height="14" width="14" />
+        <span>Icon Before</span>
+      </Button>
+      <Button optClass="secondary" collapse={true}>
+        <span>Icon After</span>
+        <Icon name="md-check" height="14" width="14" />
+      </Button>
+      <Button optClass="secondary" collapse={true}>
+        <Icon name="md-filter" height="14" width="14" />
+        <span>Icon Both</span>
+        <Icon name="md-check" height="14" width="14" />
+      </Button>
+      <Button optClass="secondary" collapse={true}>
+        <Icon name="md-filter" height="14" width="14" />
+        <span>Icon Both</span>
+        <Icon name="mbsy-caret" height="10" width="10" />
+      </Button>
+
+        <hr/>
+
+        <ExampleBottomPopover/>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./app";
 
-import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+import "./styles/styles.scss";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
