87134b92bd301e1b0969d490b517a074fe3fab82
Jean-Marc Giorgi <jmgiorgi@gmail.com>
2018-11-17 10:58:37 +1100

    app

--- src/app.js
+++ src/app.js
@@ -2,49 +2,6 @@ import React from "react";
 import Button from "./components/Button";
 import Icon from "./components/Icon";
 
-import Popover from "./components/Popover";
-import StyledDiv from "./components/StyledDiv";
-
-class ExampleBottomPopover extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  state = {
-    showing: false
-  };
-
-  togglePopover = () => {
-    this.setState({ showing: !this.state.showing });
-  };
-
-  getPopoverContent = () => (
-    <div className={localStyle["popover-wrapper"]}>
-      <h1>Lorem ipsum dolor sit amet.</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur.</p>
-      <Button onClick={this.togglePopover} optClass={localStyle["popover-btn"]}>
-        Close
-      </Button>
-    </div>
-  );
-
-  render = () => (
-    <StyledDiv css={{ display: "flex", justifyContent: "space-around" }}>
-      <Popover
-        showing={this.state.showing}
-        defaultPosition="bottom"
-        content={this.getPopoverContent()}
-        maxHeight="280px"
-        onRequestClose={this.togglePopover}
-      >
-        <Button onClick={this.togglePopover}>
-          {this.state.showing.left ? "Close" : "Open"} Bottom popover
-        </Button>
-      </Popover>
-    </StyledDiv>
-  );
-}
-
 export default () => {
   return (
     <div>
